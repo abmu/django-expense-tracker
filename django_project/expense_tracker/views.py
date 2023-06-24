48759e17af6ea35ede0ea039e0bcebb6dd0cc30f
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2023-06-24 17:08:47 +0100

    Template setup

--- django_project/expense_tracker/views.py
+++ django_project/expense_tracker/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+purchases = [
+    {
+        'user': 'AbdulM',
+        'item': 'Item 1',
+        'cost': '£4.99',
+        'date': 'June 24, 2023'
+    },
+    {
+        'user': 'AbdulM',
+        'item': 'Item 1',
+        'cost': '£4.99',
+        'date': 'June 24, 2023'
+    }
+]
+
 
 def home(request):
-    return render(request, 'expense_tracker/home.html')
+    context = {
+        'purchases': purchases
+    }
+    return render(request, 'expense_tracker/home.html', context)
 
 
 def about(request):
-    return render(request, 'expense_tracker/about.html')
+    return render(request, 'expense_tracker/about.html', {'title': 'About'})
