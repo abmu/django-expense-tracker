ec9e8164649aa460a2bbada963fa9b7979e68a86
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2023-06-26 18:01:59 +0100

    Purchases model setup

--- django_project/expense_tracker/models.py
+++ django_project/expense_tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Purchase(models.Model):
+    name = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=11, decimal_places=2)
+    date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- django_project/expense_tracker/views.py
+++ django_project/expense_tracker/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-purchases = [
-    {
-        'user': 'AbdulM',
-        'item': 'Item 1',
-        'cost': '£4.99',
-        'date': 'June 24, 2023'
-    },
-    {
-        'user': 'AbdulM',
-        'item': 'Item 1',
-        'cost': '£4.99',
-        'date': 'June 24, 2023'
-    }
-]
+from .models import Purchase
 
 
 def home(request):
     context = {
-        'purchases': purchases
+        'purchases': Purchase.objects.all()
     }
     return render(request, 'expense_tracker/home.html', context)
 
