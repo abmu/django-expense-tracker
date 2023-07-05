9471defb93b3b1e06819b45fc75a3b6171b3f46a
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2023-07-05 20:38:01 +0100

    Reset password email

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,13 +128,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 MEDIA_URL = 'media/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
-
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'tracker-home'
-
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
--- django_project/expense_tracker/urls.py
+++ django_project/expense_tracker/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', TransactionListView.as_view(), name='tracker-home'),
     path('new/', TransactionCreateView.as_view(), name='transaction-create'),
-    path('edit/<int:pk>', TransactionUpdateView.as_view(), name='transaction-update'),
-    path('delete/<int:pk>', TransactionDeleteView.as_view(), name='transaction-delete'),
+    path('edit/<int:pk>/', TransactionUpdateView.as_view(), name='transaction-update'),
+    path('delete/<int:pk>/', TransactionDeleteView.as_view(), name='transaction-delete'),
     path('about/', views.about, name='tracker-about')
 ]
